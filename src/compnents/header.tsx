35dbc4caac4480ac844b2adb5617003e866f378b
Hyunsoo Kim <rlagustn805@naver.com>
2025-03-07 17:06:42 +0900

    update - 헤더 메뉴 추가 및 반응형 작업 (햄버거 메뉴 on/off)

--- src/compnents/header.tsx
+++ src/compnents/header.tsx
@@ -4,11 +4,21 @@ import Logo from "../../public/svgs/logo.svg";
 
 export default function Header() {
   return (
-    <header className="pt-2 pb-2 border-gray-400 border-b-1 flex justify-between items-baseline">
+    <header className="pt-2 pb-2 border-gray-400 border-b-1 flex justify-between items-center">
       <Link href="/">
         <Logo />
       </Link>
-      <GiHamburgerMenu size={30} />
+
+      <nav className="hidden lg:flex gap-8 text-gray-700">
+        <Link href="/" className="hover:text-gray-900">
+          이용 안내
+        </Link>
+        <Link href="/" className="hover:text-gray-900">
+          로그인
+        </Link>
+      </nav>
+
+      <GiHamburgerMenu size={30} className="block lg:hidden" />
     </header>
   );
 }
